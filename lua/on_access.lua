786c2a7bbfb682d5ff2055963caf47f900d3936b
aifei <aifei@360.cn>
2016-03-22 19:32:23 +0800

    调整 测试
    通过了 !≈ ， ≈， = ， ！= 这些符号的测试

--- lua/on_access.lua
+++ lua/on_access.lua
@@ -4,9 +4,9 @@ local sregex  = require "lua.module.sregex"
 -- local rules = get_from_redis(ngx.var.remote_addr) -- get rules from some cache according to remote ip
 
 -- assume rules 
-local rules = '[{"act":"url","operate":"≈", "value":"hello","code":"403"}]'
+local rules = '[{"act":"url","operate":"!≈", "value":"test","code":"403"}]'
 
-local my_matcher = matcher:new(sregex.generate(rules))
-my_matcher:run()
+matcher:new(sregex.generate(rules)):run()
+
 
 
