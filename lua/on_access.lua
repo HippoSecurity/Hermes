a44cabfff00a53ca29de90b526afde25fa5a0be9
aifei <aifei@360.cn>
2016-03-22 19:57:09 +0800

    details

--- lua/on_access.lua
+++ lua/on_access.lua
@@ -4,9 +4,12 @@ local sregex  = require "lua.module.sregex"
 -- local rules = get_from_redis(ngx.var.remote_addr) -- get rules from some cache according to remote ip
 
 -- assume rules 
-local rules = '[{"act":"url","operate":"!≈", "value":"test","code":"403"}]'
+local rules = [=[
+    [
+        {"act":"url","operate":"!≈", "value":"test","code":"403"},
+        {"act":"url","operate":"≈", "value":"test1","code":"403"}
+    ]
+]=]
 
 matcher:new(sregex.generate(rules)):run()
-
-
 
