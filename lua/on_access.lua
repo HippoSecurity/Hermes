1ecb6d90529c9d9dcc5c593b8d20aa3645e2c6e1
aifei <aifei@360.cn>
2016-03-22 19:24:32 +0800

    bugfix
    这个版本能正常的跑起来了  呵呵

--- lua/on_access.lua
+++ lua/on_access.lua
@@ -4,7 +4,7 @@ local sregex  = require "lua.module.sregex"
 -- local rules = get_from_redis(ngx.var.remote_addr) -- get rules from some cache according to remote ip
 
 -- assume rules 
-local rules = '[{"act":"url","operate":"≈", "values":"hello","code":"403"}]'
+local rules = '[{"act":"url","operate":"≈", "value":"hello","code":"403"}]'
 
 local my_matcher = matcher:new(sregex.generate(rules))
 my_matcher:run()
