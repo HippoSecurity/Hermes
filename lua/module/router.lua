20ee9c4c3064e0d831ddf72f9001d34b18c82bfd
aifei <aifei@360.cn>
2016-03-23 16:21:13 +0800

    加入verynginx的前端 然后做一些修改  增加一个简单的router

--- lua/module/router.lua
+++ lua/module/router.lua
@@ -1,24 +1,63 @@
--- -- -*- coding: utf-8 -*-
--- -- -- @Date    : 2016-01-02 00:35
--- -- -- @Author  : Alexa (AlexaZhou@163.com)
--- -- -- @Link    : 
--- -- -- @Disc    : url router
+-- -*- coding: utf-8 -*-
+-- -- @Date    : 2016-01-02 00:35
+-- -- @Author  : Alexa (AlexaZhou@163.com)
+-- -- @Link    : 
+-- -- @Disc    : url router
 
 -- local summary = require "summary"
 -- local status = require "status"
 -- local cookie = require "cookie"
 -- local VeryNginxConfig = require "VeryNginxConfig"
 -- local encrypt_seed = require "encrypt_seed"
--- local cjson = require "cjson"
+local json = require "cjson"
 
--- local _M = {}
+local _M = {}
 
--- _M.url_route = {}
--- _M.mime_type = {}
--- _M.mime_type['.js'] = "application/x-javascript"
--- _M.mime_type['.css'] = "text/css"
--- _M.mime_type['.html'] = "text/html"
+_M.url_route = {}
+_M.mime_type = {}
+_M.mime_type['.js'] = "application/x-javascript"
+_M.mime_type['.css'] = "text/css"
+_M.mime_type['.html'] = "text/html"
 
+function _M.run()
+    -- body
+    local action = string.lower(ngx.req.get_method().." "..ngx.var.uri)
+
+    local handle = _M.url_route[ action ]
+    if handle ~= nil then
+        ngx.header.content_type = "application/json"
+        ngx.header.charset = "utf-8"
+        if action == "post /login" or _M.check_session() == true then
+            ngx.say( handle() )
+            ngx.exit(200)
+        else
+            local info = cjson.encode({["ret"]="failed",["err"]="need login"})
+            ngx.say( info )
+            ngx.exit(200)
+        end
+    elseif string.find(action,"get /dashboard") == 1 then
+        ngx.header.content_type = "text/html"
+        ngx.header.charset = "utf-8"
+        for k,v in pairs( _M.mime_type ) do
+            if string.sub(action, string.len(action) - string.len(k) + 1 ) == k then
+                ngx.header.content_type = v
+                break
+            end
+        end
+
+        local path = _M.root_path .."dashboard" .. string.sub( ngx.var.uri, string.len( "/dashboard") + 1 )
+
+        local f = io.open( path, 'r' )
+        if f ~= nil then
+            ngx.say( f:read("*all") )
+            f:close()
+            ngx.exit(200)
+        else        
+            ngx.exit(404)
+        end
+    end
+
+end
 -- function _M.filter() 
 --     local action = string.lower(ngx.req.get_method().." "..ngx.var.uri)
 --     local handle = _M.url_route[ action ]
@@ -85,42 +124,47 @@
 --     return false
 -- end
 
+function _M.login()  
+    -- local args = nil
+    -- local err = nil
+    -- local session = nil
 
--- function _M.login()
-    
---     local args = nil
---     local err = nil
---     local session = nil
+    -- ngx.req.read_body()
+    -- args, err = ngx.req.get_post_args()
+    -- if not args then
+    --     ngx.say("failed to get post args: ", err)
+    --     return
+    -- end
 
---     ngx.req.read_body()
---     args, err = ngx.req.get_post_args()
---     if not args then
---         ngx.say("failed to get post args: ", err)
---         return
---     end
-
---     for i,v in ipairs( VeryNginxConfig.configs['admin'] ) do
---         if v['user'] == args['user'] and v['password'] == args["password"] and v['enable'] == true then
---             session = ngx.md5(encrypt_seed.get_seed()..v['user'])
---             ngx.header['Set-Cookie'] = {
---                 string.format("verynginx_session=%s; path=/verynginx", session ),
---                 string.format("verynginx_user=%s; path=/verynginx", v['user'] ),
---             }
+    -- for i,v in ipairs( VeryNginxConfig.configs['admin'] ) do
+    --     if v['user'] == args['user'] and v['password'] == args["password"] and v['enable'] == true then
+    --         session = ngx.md5(encrypt_seed.get_seed()..v['user'])
+    --         ngx.header['Set-Cookie'] = {
+    --             string.format("verynginx_session=%s; path=/verynginx", session ),
+    --             string.format("verynginx_user=%s; path=/verynginx", v['user'] ),
+    --         }
             
---             return cjson.encode({["ret"]="success",["err"]=err})
---         end
---     end 
+    --         return cjson.encode({["ret"]="success",["err"]=err})
+    --     end
+    -- end 
     
---     return cjson.encode({["ret"]="failed",["err"]=err})
+    -- return cjson.encode({["ret"]="failed",["err"]=err})
+    ngx.log(ngx.ERR, "fucking herer!!!")
+    return json.encode({["ret"]="success",["err"]=err})
+end
 
--- end
+function _M.home_path()
+    local current_script_path = debug.getinfo(1, "S").source:sub(2)
+    local home_path = current_script_path:sub( 1, 0 - string.len("/lua/module/router.lua") -1 ) 
+    return home_path
+end
 
-
--- _M.url_route["post /verynginx/login"] = _M.login
+_M.root_path = _M.home_path()
+_M.url_route["post /login"] = _M.login
 -- _M.url_route["get /verynginx/summary"] = summary.report
 -- _M.url_route["get /verynginx/status"] = status.report
 -- _M.url_route["get /verynginx/config"] = VeryNginxConfig.report
 -- _M.url_route["post /verynginx/config"] = VeryNginxConfig.set
 -- _M.url_route["get /verynginx/loadconfig"] = VeryNginxConfig.load_from_file
 
--- return _M
+return _M
